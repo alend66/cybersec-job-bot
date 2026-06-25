15cc405d9e5a7bfb0f8bf3d85b112932d252c518
alend66 <92348030+alend66@users.noreply.github.com>
2026-06-26 02:53:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+import asyncio
 from telegram.ext import Application
 from config import BOT_TOKEN
 from modules.telegram_bot import register_handlers
 
-def main():
-    if not BOT_TOKEN:
-        print("❌ BOT_TOKEN not found. Check your .env file.")
-        return
-
+async def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     register_handlers(app)
 
-    print("🚀 CyberSec Job Bot Started...")
+    print("🚀 CyberSec Job Bot Started")
 
-    app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    try:
+        await asyncio.Event().wait()
+    finally:
+        await app.updater.stop()
+        await app.stop()
+        await app.shutdown()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
