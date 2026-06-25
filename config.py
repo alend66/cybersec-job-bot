5eb21e26d987bc74b860722482fdf358c9a5ff52
alend66 <92348030+alend66@users.noreply.github.com>
2026-06-26 02:28:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv(8861055827:AAFpv-nagWEmC40btGd-t4imggHOVpW-Yeo)
-OPENAI_API_KEY = os.getenv(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)
+OPENAI_API_KEY = os.getenv(sk-proj-gs-uxUf5Z6NGMSLjXCPzUiwIPcOMG44KidwP111VcBFerocAufFDw5o1JzNAvLjYkt2xqGgV3tT3BlbkFJt2KF5mYXON28mV08PT8HezdoPiCwsOWXylOTsmDRoA2UjVzfOUC7F009xYIbhFziESqDS-cAE)
 DATABASE = os.getenv(jobs.db)
 ADMIN_ID = os.getenv(8353601664)
