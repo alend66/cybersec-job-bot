4ec178821efbb45da756763eceefe66b7a1c1c36
alend66 <92348030+alend66@users.noreply.github.com>
2026-06-26 01:30:36 +0530

    Create jobs.py

---
+++ modules/jobs.py
@@ -0,0 +1,8 @@
+def get_latest_jobs():
+    return [
+        {
+            "company": "Coming Soon",
+            "role": "Cyber Security Analyst",
+            "location": "India"
+        }
+    ]
