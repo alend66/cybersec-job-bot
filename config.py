68bbec133fb4e149ceeadc52b8dc97cf5aade903
alend66 <92348030+alend66@users.noreply.github.com>
2026-06-26 01:17:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = os.getenv(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)
 DATABASE = os.getenv("DATABASE")
 ADMIN_ID = os.getenv("ADMIN_ID")
