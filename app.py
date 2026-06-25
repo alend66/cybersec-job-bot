b892d14fc2da1a5ff46717dee66497173993b1b4
alend66 <92348030+alend66@users.noreply.github.com>
2026-06-26 02:20:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("CyberSec Job Bot Started")
+from telegram.ext import Application
+from config import BOT_TOKEN
+from modules.telegram_bot import register_handlers
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    register_handlers(app)
+
+    print("🚀 CyberSec Job Bot Started...")
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
